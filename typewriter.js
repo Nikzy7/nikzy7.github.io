44adb2f2d8a09d3560062211df1036e12fafac28
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-12 00:10:13 +0530

    add typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+let el = document.querySelector(".typewriter");
+
+var data = "Incoming SDE Intern at Walmart Global Tech India";
+
+var index = 0;
+setInterval(() => {
+    if(index==data.length){
+        el.innerHTML = data;
+        return;
+    }else{
+        let currentData = el.innerHTML;
+        currentData = currentData.slice(0,currentData.length-2);
+        // console.log(currentData);
+        let toWrite = currentData.concat(data[index].concat(" |"));
+        el.innerHTML = toWrite;
+        index = index+1;
+    }
+},100);
